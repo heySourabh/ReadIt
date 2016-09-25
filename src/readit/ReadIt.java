b452ee413aadb465fe37668b775758ad02d6fddd
Sourabh Bhat <heySourabh@gmail.com>
2016-09-26 02:51:10 +0530

    Graphical user interface for the program
    - Added a new class for graphical user interface.
    - Now overcoming the problem with modifier keys being pressed, by
      forcing the release of modifier keys at the time of launching.
    - Added distribution package in dist folder for usage without
      a need of compilation.

--- src/readit/ReadIt.java
+++ src/readit/ReadIt.java
@@ -1,6 +1,7 @@
 package readit;
 
 import java.awt.AWTException;
+import java.awt.HeadlessException;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
@@ -9,19 +10,41 @@ import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.locks.LockSupport;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class ReadIt {
+
+    static Media media;
+    static MediaPlayer mediaPlayer;
+
     public static void main(String[] args) throws Exception {
+        // release all modifier keys (alt, ctrl, shift)
+        releaseAllModifierKeys();
+
         // Copy selection to clipboard
         copySelection();
-        
+
         // Get the string from clipboard
         String text = getStringFromClipboard();
-        
+
         // Sent the string to tts engine to read it out
-        speakIt(text);
+        new Thread(() -> speakIt(text)).start();
+
+        // display the user inteface
+        new UserInterface().display(args);
     }
-    
+
+    private static void releaseAllModifierKeys() throws AWTException {
+        Robot robot = new Robot();
+        robot.keyRelease(KeyEvent.VK_ALT);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        robot.keyRelease(KeyEvent.VK_SHIFT);
+    }
+
     private static void copySelection() throws AWTException {
         Robot robot = new Robot();
         robot.keyPress(KeyEvent.VK_CONTROL);
@@ -29,25 +52,39 @@ public class ReadIt {
         robot.keyRelease(KeyEvent.VK_C);
         robot.keyRelease(KeyEvent.VK_CONTROL);
     }
-    
-    private static String getStringFromClipboard() throws IOException, UnsupportedFlavorException {
-        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-        return (String)clipboard.getData(DataFlavor.stringFlavor);
+
+    private static String getStringFromClipboard() {
+        try {
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            return (String) clipboard.getData(DataFlavor.stringFlavor);
+        } catch (HeadlessException | UnsupportedFlavorException | IOException ex) {
+            return "Sorry! I was unable to read from the clipboard.";
+        }
     }
-    
-    private static void speakIt(String text) throws IOException, InterruptedException {
-        String fileName = new File("./tmp.wav").getAbsolutePath();
-        Process proc = Runtime.getRuntime().exec(new String[]{
-            "pico2wave",
-            "-w", fileName,
-            text
-        });
-        proc.waitFor();
-        
-        proc = Runtime.getRuntime().exec(new String[]{
-            "aplay", 
-            fileName
-        });
-        proc.waitFor();
+
+    private static void speakIt(String text) {
+        try {
+            File file = File.createTempFile("speech", ".wav");
+            String fileName = file.getAbsolutePath();
+            Process proc = Runtime.getRuntime().exec(new String[]{
+                "pico2wave",
+                "-w", fileName,
+                text
+            });
+            proc.waitFor();
+
+            media = new Media(file.toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+            mediaPlayer.play();
+        } catch (IOException | InterruptedException ex) {
+            LockSupport.parkNanos(1_000_000_000);
+            Platform.runLater(() -> {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Unable to create speech from text!");
+                alert.setTitle("TTS error");
+                alert.showAndWait();
+                System.exit(1);
+            });
+        }
     }
 }
---
+++ src/readit/UserInterface.java
@@ -0,0 +1,167 @@
+package readit;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.effect.Glow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+import static readit.ReadIt.mediaPlayer;
+
+public class UserInterface extends Application {
+
+    final static String PAUSE_IMG_PATH = "/images/pause.png";
+    final static String PLAY_IMG_PATH = "/images/play.png";
+    final static String ICON_IMG_PATH = "/images/icon_64x64.png";
+
+    final static Image PAUSE_IMG;
+    final static Image PLAY_IMG;
+    final static Image ICON_IMG;
+
+    boolean playing = true;
+    double mouseLocX = 0.0;
+    double mouseLocY = 0.0;
+
+    ImageView control;
+    boolean mouseDragged = false;
+
+    static {
+        PAUSE_IMG = new Image(ReadIt.class
+                .getResourceAsStream(PAUSE_IMG_PATH));
+        PLAY_IMG = new Image(ReadIt.class
+                .getResourceAsStream(PLAY_IMG_PATH));
+        ICON_IMG = new Image(ReadIt.class
+                .getResourceAsStream(ICON_IMG_PATH));
+    }
+
+    public void display(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        control = new ImageView(PAUSE_IMG);
+        control.setOnMouseEntered(e -> control.setEffect(new Glow(0.25)));
+        control.setOnMouseExited(e -> control.setEffect(new Glow(0.0)));
+        control.setOnMousePressed(e -> mousePressed(e, primaryStage));
+        control.setOnMouseReleased(e -> mouseReleased(e));
+        control.setOnMouseDragged(e -> mouseDragged(e, primaryStage));
+        control.setOnScroll(e -> mouseScrolled(e));
+
+        Group container = new Group(control);
+        Scene scene = new Scene(container, Color.TRANSPARENT);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+
+        primaryStage.setScene(scene);
+        primaryStage.setAlwaysOnTop(true);
+        primaryStage.setMinWidth(PAUSE_IMG.getWidth());
+        primaryStage.setMaxWidth(PAUSE_IMG.getWidth());
+        primaryStage.setMinHeight(PAUSE_IMG.getHeight());
+        primaryStage.setMaxHeight(PAUSE_IMG.getHeight());
+        primaryStage.getIcons().add(ICON_IMG);
+        primaryStage.setTitle("Read It");
+        primaryStage.show();
+    }
+
+    private void play() {
+        if (mediaPlayer == null) {
+            return;
+        }
+        control.setImage(PAUSE_IMG);
+        mediaPlayer.seek(mediaPlayer.getCurrentTime().subtract(Duration.seconds(1.0)));
+        mediaPlayer.play();
+        playing = true;
+    }
+
+    private void pause() {
+        if (mediaPlayer == null) {
+            return;
+        }
+        control.setImage(PLAY_IMG);
+        mediaPlayer.pause();
+        playing = false;
+    }
+
+    private void mousePressed(MouseEvent e, Stage stage) {
+        getContextMenu().hide();
+        if (e.getButton() != MouseButton.PRIMARY) {
+            return;
+        }
+        mouseDragged = false;
+        control.setEffect(new Glow(0.5));
+        mouseLocX = stage.getX() - e.getScreenX();
+        mouseLocY = stage.getY() - e.getScreenY();
+    }
+
+    private void mouseReleased(MouseEvent e) {
+        if (e.getButton() == MouseButton.SECONDARY) {
+            getContextMenu().show(control, e.getScreenX(), e.getScreenY());
+        }
+        if (e.getButton() != MouseButton.PRIMARY) {
+            return;
+        }
+        control.setEffect(new Glow(0.25));
+        if (mouseDragged) {
+            return;
+        }
+        if (playing) {
+            pause();
+        } else {
+            play();
+        }
+    }
+
+    private void mouseDragged(MouseEvent e, Stage stage) {
+        if (e.getButton() != MouseButton.PRIMARY) {
+            return;
+        }
+        mouseDragged = true;
+        double curLocX = stage.getX() - e.getScreenX();
+        double curLocY = stage.getY() - e.getScreenY();
+
+        stage.setX(stage.getX() + mouseLocX - curLocX);
+        stage.setY(stage.getY() + mouseLocY - curLocY);
+    }
+
+    private void mouseScrolled(ScrollEvent e) {
+        double direction = Math.signum(e.getDeltaY());
+        mediaPlayer.seek(mediaPlayer.getCurrentTime().add(Duration.seconds(direction * 0.5)));
+    }
+
+    ContextMenu menu;
+
+    private ContextMenu getContextMenu() {
+        if (menu == null) {
+            MenuItem exitItem = new MenuItem("Exit");
+            exitItem.setOnAction(e -> System.exit(0));
+
+            MenuItem aboutItem = new MenuItem("About...");
+            aboutItem.setOnAction(e -> showAboutDialog());
+            menu = new ContextMenu(aboutItem, exitItem);
+        }
+        return menu;
+    }
+
+    private void showAboutDialog() {
+        Alert aboutDialog = new Alert(Alert.AlertType.INFORMATION);
+        aboutDialog.setTitle("About");
+        aboutDialog.setHeaderText("About");
+        aboutDialog.setGraphic(new ImageView(ICON_IMG));
+        aboutDialog.setContentText(
+                "This is a convenient text to speech (TTS) program.\n"
+                + "Programmed by: \n"
+                + "Sourabh Bhat (heySourabh@gmail.com)");
+        aboutDialog.showAndWait();
+    }
+}
